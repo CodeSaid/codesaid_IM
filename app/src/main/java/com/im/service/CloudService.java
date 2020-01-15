5cbbe907abe08868da13615ef1b08da087cbbf20
CodeSaid <codesaid526@gmail.com>
2020-01-16 00:24:02 +0800

    接收好友消息

--- app/src/main/java/com/im/service/CloudService.java
+++ app/src/main/java/com/im/service/CloudService.java
@@ -3,16 +3,20 @@ package com.im.service;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.text.TextUtils;
 
 import androidx.annotation.Nullable;
 
+import com.codesaid.lib_framework.bean.TextBean;
 import com.codesaid.lib_framework.cloud.CloudManager;
 import com.codesaid.lib_framework.entity.Constants;
 import com.codesaid.lib_framework.utils.log.LogUtils;
 import com.codesaid.lib_framework.utils.sp.SpUtils;
+import com.google.gson.Gson;
 
 import io.rong.imlib.RongIMClient;
 import io.rong.imlib.model.Message;
+import io.rong.message.TextMessage;
 
 /**
  * Created By codesaid
@@ -49,6 +53,26 @@ public class CloudService extends Service {
             @Override
             public boolean onReceived(Message message, int i) {
                 LogUtils.e("message: " + message);
+                String objectName = message.getObjectName();
+
+                if (objectName.equals(CloudManager.MSG_TEXT_NAME)) { // 文本消息
+                    // 获取消息主体
+                    TextMessage textMessage = (TextMessage) message.getContent();
+                    String content = textMessage.getContent();
+                    LogUtils.i("content: " + content);
+                    if (!TextUtils.isEmpty(content)) {
+                        TextBean textBean = new Gson().fromJson(content, TextBean.class);
+
+                        if (textBean.getType().equals(CloudManager.TYPE_TEXT)) { // 普通消息
+
+                        } else if (textBean.getType().equals(CloudManager.TYPE_ADD_FRIEND)) { // 添加好友消息
+                            // 存入数据库
+                            LogUtils.i("收到添加好友消息");
+                        } else if (textBean.getType().equals(CloudManager.TYPE_ARGEED_FRIEND)) { // 同意添加好友消息
+
+                        }
+                    }
+                }
                 return false;
             }
         });
