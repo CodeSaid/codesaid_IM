9191692ccb6caae7f29aaa92c7d592e1a00c38a4
CodeSaid <codesaid526@gmail.com>
2020-02-09 19:57:18 +0800

    分享个人信息----> 保存图片到相册并且刷新

--- app/src/main/java/com/im/ui/ShareImageActivity.java
+++ app/src/main/java/com/im/ui/ShareImageActivity.java
@@ -1,8 +1,13 @@
 package com.im.ui;
 
 import android.annotation.SuppressLint;
+import android.content.ContentValues;
+import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -12,8 +17,15 @@ import com.codesaid.lib_framework.base.BaseBackActivity;
 import com.codesaid.lib_framework.bmob.BmobManager;
 import com.codesaid.lib_framework.bmob.IMUser;
 import com.codesaid.lib_framework.helper.GlideHelper;
+import com.codesaid.lib_framework.utils.toast.ToastUtils;
+import com.codesaid.lib_framework.view.LoadingView;
 import com.im.R;
 import com.uuzuche.lib_zxing.activity.CodeUtils;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 /**
  * Created By codesaid
@@ -42,6 +54,8 @@ public class ShareImageActivity extends BaseBackActivity implements View.OnClick
     //下载
     private LinearLayout ll_download;
 
+    private LoadingView mLoadingView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,6 +66,9 @@ public class ShareImageActivity extends BaseBackActivity implements View.OnClick
     }
 
     private void initView() {
+
+        mLoadingView = new LoadingView(ShareImageActivity.this);
+        mLoadingView.setLoadingText(getString(R.string.text_shar_save_ing));
 
         iv_photo = findViewById(R.id.iv_photo);
         tv_name = findViewById(R.id.tv_name);
@@ -108,7 +125,85 @@ public class ShareImageActivity extends BaseBackActivity implements View.OnClick
         switch (view.getId()) {
             case R.id.ll_download:
 
+                /**
+                 * 1.View截图
+                 * 2.创建一个Bitmap
+                 * 3.保存到相册
+                 */
+                mLoadingView.show();
+
+                /**
+                 * setDrawingCacheEnabled
+                 * 保留我们的绘制副本
+                 * 1.重新测量
+                 * 2.重新布局
+                 * 3.得到我们的DrawingCache
+                 * 4.转换成Bitmap
+                 */
+
+                ll_content.setDrawingCacheEnabled(true);
+
+
+                ll_content.measure(View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED),
+                        View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED));
+
+                ll_content.layout(0, 0, ll_content.getMeasuredWidth(),
+                        ll_content.getMeasuredHeight());
+
+                Bitmap bitmap = ll_content.getDrawingCache();
+
+                if (bitmap != null) {
+                    saveBitmapToAlbum(bitmap);
+                }
                 break;
         }
+    }
+
+    private void saveBitmapToAlbum(Bitmap bitmap) {
+        File rootPath = new File(Environment.getExternalStorageDirectory() + "/codesaid_IM/");
+
+        if (!rootPath.exists()) {
+            rootPath.mkdirs();
+        }
+
+        File file = new File(rootPath, System.currentTimeMillis() + ".png");
+
+        try {
+            FileOutputStream outputStream = new FileOutputStream(file);
+
+            // 自带的保存方法
+            bitmap.compress(Bitmap.CompressFormat.PNG, 90, outputStream);
+            outputStream.flush();
+            outputStream.close();
+
+            mLoadingView.hide();
+            ToastUtils.show(ShareImageActivity.this, "保存成功");
+
+            updateAlbum(file.getPath());
+        } catch (IOException e) {
+            e.printStackTrace();
+            mLoadingView.hide();
+            ToastUtils.show(ShareImageActivity.this, "保存失败");
+        }
+    }
+
+    /**
+     * 刷新图库
+     */
+    private void updateAlbum(String path) {
+        /**
+         * 存在兼容性问题
+         */
+
+        // 通过广播刷新
+        sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, Uri.parse(path)));
+
+        // 通过数据库的方式插入
+        ContentValues values = new ContentValues(4);
+        values.put(MediaStore.Video.Media.TITLE, "");
+        values.put(MediaStore.Video.Media.MIME_TYPE, "image/jpeg");
+        values.put(MediaStore.Video.Media.DATA, path);
+        values.put(MediaStore.Video.Media.DURATION, 0);
+        getContentResolver().insert(MediaStore.Video.Media.EXTERNAL_CONTENT_URI, values);
     }
 }
